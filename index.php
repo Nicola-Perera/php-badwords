99e8163270ff3b398566edc239bcb734ef240a74
Nicola-Perera <nicola.perera96@mail.com>
2021-12-01 17:18:34 +0100

    add variable with manual string by queries

--- index.php
+++ index.php
@@ -10,4 +10,7 @@
 $text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, repudiandae consectetur. Quibusdam, optio nemo beatae unde ipsum soluta voluptatem illo aspernatur deleniti sequi neque quis! Ea laudantium voluptate temporibus molestias! Provident voluptas a cupiditate odit, accusamus voluptate? Unde iste molestias eos alias? In praesentium totam magnam cupiditate explicabo laborum porro voluptatum libero. Alias sit reiciendis harum, iusto ab incidunt rem consequatur. Laborum inventore quo numquam quas voluptatum provident non. Doloremque architecto dolores doloribus! Illum vitae voluptate ducimus nesciunt voluptas accusamus, ullam ut assumenda tenetur libero necessitatibus ratione quae magni alias provident impedit quo maiores esse. Rerum voluptatum exercitationem distinctio. Consequuntur.';
 
 var_dump($text);
+
+$banned_word = $_GET['ban'];
+var_dump('hai proibito la parola: ' . $banned_word);
 ?>
