4f8e8cdf66e3c963adbd615de2cc58971aa4f5bf
Nicola-Perera <nicola.perera96@mail.com>
2021-12-01 16:57:25 +0100

    add variable with text and print it on screen

--- index.php
+++ index.php
@@ -7,4 +7,7 @@
         Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
     */
 
+$text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, repudiandae consectetur. Quibusdam, optio nemo beatae unde ipsum soluta voluptatem illo aspernatur deleniti sequi neque quis! Ea laudantium voluptate temporibus molestias! Provident voluptas a cupiditate odit, accusamus voluptate? Unde iste molestias eos alias? In praesentium totam magnam cupiditate explicabo laborum porro voluptatum libero. Alias sit reiciendis harum, iusto ab incidunt rem consequatur. Laborum inventore quo numquam quas voluptatum provident non. Doloremque architecto dolores doloribus! Illum vitae voluptate ducimus nesciunt voluptas accusamus, ullam ut assumenda tenetur libero necessitatibus ratione quae magni alias provident impedit quo maiores esse. Rerum voluptatum exercitationem distinctio. Consequuntur.';
+
+var_dump($text);
 ?>
